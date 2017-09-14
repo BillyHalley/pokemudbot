c9ff45a6eba12a6e5297ad85d9a86cd35164b82e
BillyHalley <ziobilly94@gmail.com>
2017-09-14 22:24:33 +0000

    Added commands in rooms descriptions

--- kanto.py
+++ kanto.py
@@ -2,6 +2,6 @@ from classes import Room
 
 kanto = {}
 kanto['pallet_town'] = Room(
-    "Pallet Town", "You just moved here, there is your house, along with other three.\nA route starts from the north.", 'route1', None, None, None, None, None)
-kanto['route1'] = Room("Route 1", "Route from Pallet Town to Viridian City.",
+    "Pallet Town", "You just moved here, there is your house, along with other three.\nA route starts from the /north.", 'route1', None, None, None, None, None)
+kanto['route1'] = Room("Route 1", "Route from Pallet Town to Viridian City.\nGoing /south you can reach Pallet Town",
                        None, 'pallet_town', None, None, None, None)
